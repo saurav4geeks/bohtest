51e641a64a992e49b3967918eb6534e9e6c1c5a9
Saurav Suman <74022352+saurav4geeks@users.noreply.github.com>
2021-02-27 22:20:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var score1 = document.querySelector('.score1'); //emp
+var score2 = document.querySelector('.score2'); //syn
+var score3 = document.querySelector('.score3'); //bo
+var score4 = document.querySelector('.score4'); //loa
+var totevent = document.querySelector('.event');
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://boh.froshtiet.com/mentorsonly/score');
+ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText); 
+    console.log(ourData.clans[0].name);
+    for(let i=0; i<4; i++){
+        if(ourData.clans[i].name === "theEmpire") score1.textContent = ourData.clans[i].score;
+        else if(ourData.clans[i].name === "theSyndicate") score2.textContent = ourData.clans[i].score;
+        else if(ourData.clans[i].name === "blackOrder") score3.textContent = ourData.clans[i].score;
+        else if(ourData.clans[i].name === "theLeagueOfAssassins") score4.textContent = ourData.clans[i].score;
+    }
+    for(var i=0; i<ourData.events.length; i++){
+        totevent.innerHTML += "<a href=" +ourData.events[i].link + ">" + ourData.events[i].name + "</a>"
+    }
+};
+
+
+ourRequest.send();
