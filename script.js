90421c286ec1cf87b5f52a32bf805c83becfea11
Saurav Suman <74022352+saurav4geeks@users.noreply.github.com>
2021-03-09 17:45:30 +0530

    updated event header

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var score1 = document.querySelector('.score1'); //emp
 var score2 = document.querySelector('.score2'); //syn
 var score3 = document.querySelector('.score3'); //bo
 var score4 = document.querySelector('.score4'); //loa
-var totevent = document.querySelector('.event');
+var totevent = document.querySelector('.event_para');
+var event_header = document.querySelector('.event_header');
 
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://boh.froshtiet.com/mentorsonly/score');
@@ -16,9 +17,14 @@ ourRequest.onload = function(){
         else if(ourData.clans[i].name === "theLeagueOfAssassins") score4.textContent = ourData.clans[i].score;
     }
     for(var i=0; i<ourData.events.length; i++){
-        totevent.innerHTML += "<a href=" +ourData.events[i].link + ">" + ourData.events[i].name + "</a>"
+        event_header.innerHTML += "<h1 class='event-head-js'>"+ ourData.events[i].header_name + "</h1>"
+    }
+    for(var i=0; i<ourData.events.length; i++){
+        totevent.innerHTML += "<a target=”_blank” href=" +ourData.events[i].link + ">" + ourData.events[i].name + "</a>"
     }
 };
 
 
 ourRequest.send();
+
+// header_name ka data send krna hai
